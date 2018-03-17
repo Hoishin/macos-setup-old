6275cd4b6d4405ba969455b3925229dd7b806d4a
Hoishin <hoishinxii@gmail.com>
2018-03-18 08:55:18 +0900

    Install yarn separately --without-node

--- bin/brew.sh
+++ bin/brew.sh
@@ -5,6 +5,7 @@ CONFIG_DIR=$DIRNAME/../config
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
+brew install yarn --without-node
 brew install $( cat $CONFIG_DIR/brew )
 brew tap caskroom/cask
 brew cask install $( cat $CONFIG_DIR/brew-cask )
