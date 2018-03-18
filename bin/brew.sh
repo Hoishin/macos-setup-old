b04f23cc209b66b54690bb1eba6d569100ccef3a
Hoishin <hoishinxii@gmail.com>
2018-03-18 17:43:52 +0900

    Include yarn installation

--- bin/brew.sh
+++ bin/brew.sh
@@ -5,7 +5,6 @@ CONFIG_DIR=$DIRNAME/../config
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
-brew install yarn --without-node
-brew install $( cat $CONFIG_DIR/brew )
+cat $CONFIG_DIR/brew | xargs -L1 brew install
 brew tap caskroom/cask
 brew cask install $( cat $CONFIG_DIR/brew-cask )
