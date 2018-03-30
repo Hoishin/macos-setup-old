85fc62f8ae69f626c5e8c8e1fb0b748dd7b36398
Keiichiro Amemiya <hoishinxii@gmail.com>
2018-03-31 03:09:56 +0900

    Update brew.sh

--- bin/brew.sh
+++ bin/brew.sh
@@ -5,6 +5,6 @@ CONFIG_DIR=$DIRNAME/../config
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
-cat $CONFIG_DIR/brew | xargs -L1 brew install
 brew tap caskroom/cask
 brew cask install $( cat $CONFIG_DIR/brew-cask )
+cat $CONFIG_DIR/brew | xargs -L1 brew install
